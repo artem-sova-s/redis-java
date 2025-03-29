5c58c30eefca883c7da822c16f1da19cb9f2d068
githubuser402 <colomborin53@gmail.com>
2025-03-29 02:25:08 +0200

    added CommandContext that contains information about user command

--- src/main/java/command/CommandContext.java
+++ src/main/java/command/CommandContext.java
@@ -1,45 +1,47 @@
-package network.handler;
+package command;
 
-import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class CommandContext {
-    private InputStream inputStream = null;
+    private static final Logger log = LoggerFactory.getLogger(CommandContext.class);
+
     private OutputStream outputStream = null;
     private String command;
-
-    // executed command counter
-    // used to check the count of executed commands in the pipeline
-    private int executedCommandCounter = 0;
 
     // list of arguments for the command execution
     private List<String> commandArgs = null;
 
-    public CommandContext(String command, InputStream in, OutputStream out, List<String> args) {
+    public CommandContext(String command, OutputStream out, List<String> args) {
+        log.info("New CommandContext for command: " + command);
+
         this.command = command;
-        this.inputStream = in;
         this.outputStream = out;
         this.commandArgs = args;
     }
 
-    public void incrementExecutedCommandCounter() {
-        executedCommandCounter++;
+    /*
+     * This overrided constructor is used for NULL object
+     * for the case when command is invalid or empty
+     */
+    public CommandContext(final OutputStream out) {
+        log.info("Nullable CommandContext created");
+        this.command = null;
+        this.outputStream = out;
     }
 
-    public int getExecutedCommandCounter() {
-        return executedCommandCounter;
-    }
-
-    public InputStream getInputStream() {
-        return inputStream;
-    }
-    
     public OutputStream getOutputStream() {
         return outputStream;
     }
-    
+
     public String getCommand() {
         return command;
+    }
+
+    public List<String> getCommandArgs() {
+        return commandArgs;
     }
 }
