7345985c88825a25728ea5d0d1fa1be059a61023
githubuser402 <colomborin53@gmail.com>
2025-03-29 02:25:52 +0200

    added CommandParser to parse user input into CommandContext

--- src/main/java/command/CommandParser.java
+++ src/main/java/command/CommandParser.java
@@ -1,4 +1,36 @@
 package command;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.List;
+
+// class is dedicad for parsing the input stream
+// validation of the command correctness
+// returns CommandContext
 public class CommandParser {
+    private static final Logger log = LoggerFactory.getLogger(CommandParser.class);
+
+    public CommandContext parse(final String clientMessage, final OutputStream outputStream) throws IOException {
+        try {
+
+            if (clientMessage == null || clientMessage.trim().isEmpty()) {
+                return new CommandContext(outputStream);
+            }
+
+            log.info("Parsing command: " + clientMessage);
+
+            String[] tokens = clientMessage.trim().split("\\s+");
+            String command = tokens[0];
+            List<String> args = Arrays.asList(tokens).subList(1, tokens.length);
+
+            // creation of the CommandContext object with parsed data
+            return new CommandContext(command, outputStream, args);
+        } catch (Exception e) {
+            log.error("Error reading command line", e);
+            throw e;
+        }
+    }
 }
